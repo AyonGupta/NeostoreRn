93721bd1f7ccf00624f0e6cae57ef256d0bb7680
AyonGupta <ayon.gupta@wwindia.com>
2021-02-12 14:36:58 +0530

    Integrated Thunk

--- App.js
+++ App.js
@@ -1,18 +1,60 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow strict-local
+*/
 
 import React from 'react';
 import ProductList from "./Views/ProductList";
-
-
+import { useDispatch, useSelector } from "react-redux";
+import { PerformLogin } from "./Redux/Action/LoginAction";
+import 
+{ 
+  View ,
+  Button,
+  Text
+} from "react-native";
 const App = () => {
+  
+  const dispatch = useDispatch ()
+  const isLogin = useSelector (state => state.loginReducer.isLogin)
+  const isLoading = useSelector (state => state.loginReducer.isLoader)
+
+  console.log ('isLoading =', isLoading)
   return (
-    <ProductList/>
-  );
-}
-export default App;
+    <View style = 
+    {
+      {
+        height : '100%',
+        justifyContent : 'center',
+        alignItems : 'center'
+      }
+    }>
+    <Button 
+    title = "Login" 
+    style = 
+    {
+      {
+        
+      }
+    }
+    onPress = 
+    {
+      () => 
+      {
+        dispatch (PerformLogin ("a@e.in", "1234"))
+      }
+    }/>
+    
+    {/* <Text> {isLoading ? 'Loading...' : ''}</Text> */}
+    {isLoading && <Text>Loading</Text>}
+    {/* <Text>Hello world</Text> */}
+    {/* <Text>  {isLogin ? 'Login success' : ''}</Text> */}
+    </View>
+    //<ProductList/>
+    );
+  }
+  export default App;
+  
