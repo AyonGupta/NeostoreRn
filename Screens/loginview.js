8c163ed2373bdc5872b3d20635691b06edbee545
Priya <kamblepriya49@gmail.com>
2021-03-03 12:09:41 +0530

    Changes for  UI

--- Screens/loginview.js
+++ Screens/loginview.js
@@ -1,48 +1,81 @@
-import React,{ Component } from 'react';
+import React,{ useState } from 'react'; 
 import{
     View,SafeAreaView,Text,TextInput,StyleSheet,Button, TouchableOpacity,Image
 } from 'react-native';
 import ButtonView from '../Helper/buttonview';
 import TextInputView  from '../Helper/textinputview';
 
-export class LoginView extends Component
+const LoginView = () =>
 {
-    render()
+    let[Username,setUserName] = useState('');
+    let[Password,setPassword] = useState('');
+
+    //Email Validation
+    const ValidateEmail = () =>
     {
-        return(
-            <SafeAreaView style = {styles.maincontainerStyle}>
-                <View style = {styles.containerStyle}>
-                    <Text style = {styles.textStyle}>NeoSTORE</Text>
-                        <TextInputView source ={require('../Resources/user.png')}  placeholder = 'Username' placeholderTextColor = 'white'/>
-                        <TextInputView source ={require('../Resources/password.png')} placeholder = 'Password' placeholderTextColor = 'white'/>
-                        <ButtonView title = 'LOGIN'/>
+        // var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+        // if (!pattern.test(input["Username"])) {
+        //   isValid = false;
+        //   errors["email"] = "Please enter valid email address.";
+        // }
+    };
+
+    //Username and password validation
+    const handleLogin = () =>
+    {
+        console.log(Username,Password);
+        if(!Username){
+            alert('Please enter username.');
+            return;
+        }
+        else if(!Password && Password.trim() && Password.length > 6)
+        {
+            alert('Please enter valid password.')
+            return;
+        }
+        else if(!ValidateEmail)
+        {
+            alert('Please enter valid email.')
+            return;
+        }
+        else{
+
+        }
+    };
+
+    return(
+        <SafeAreaView style = {styles.maincontainerStyle}>
+            <View style = {styles.containerStyle}>
+                <Text style = {styles.textStyle}>NeoSTORE</Text>
+                    <TextInputView source ={require('../Resources/user.png')}  placeholder = 'Username' placeholderTextColor = 'white' onChangeText = {(Username) => setUserName(Username)}/>
+                    <TextInputView source ={require('../Resources/password.png')} placeholder = 'Password' placeholderTextColor = 'white' onChangeText = {(Password) => setPassword(Password)}/>
+                    <ButtonView title = 'LOGIN' onPress = {handleLogin}/>
 
 
-                    <TouchableOpacity>
-                        <Text style={{alignSelf:'center', alignItems :'center',
-                        justifyContent:'center',
-                        color:'white',
-                        marginTop: 10,
-                        fontWeight : 'bold',
-                        fontSize: 20}}>Forgot Password ?</Text>
-                    </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={{alignSelf:'center', alignItems :'center',
+                    justifyContent:'center',
+                    color:'white',
+                    marginTop: 10,
+                    fontWeight : 'bold',
+                    fontSize: 20}}>Forgot Password ?</Text>
+                </TouchableOpacity>
 
+            </View>
+            <View style = {styles.labelinputcontainerStyle}>
+                <TouchableOpacity>
+                    <Text style={{alignSelf:'flex-start', alignItems :'center', textAlignVertical : 'bottom',
+                    justifyContent:'center',
+                    color:'white',
+                    fontWeight : 'bold',
+                    fontSize: 15}}>DON'T HAVE AN ACCOUNT?</Text>
+                </TouchableOpacity>
+                <Image style={{justifyContent : 'center' ,
+                alignSelf :'flex-end',height : 30,width :30,alignItems:'flex-end'
+                }} source = {require('../Resources/plus.png')} ></Image>
                 </View>
-                <View style = {styles.labelinputcontainerStyle}>
-                    <TouchableOpacity>
-                        <Text style={{alignSelf:'flex-start', alignItems :'center', textAlignVertical : 'bottom',
-                        justifyContent:'center',
-                        color:'white',
-                        fontWeight : 'bold',
-                        fontSize: 15}}>DON'T HAVE AN ACCOUNT?</Text>
-                    </TouchableOpacity>
-                    <Image style={{justifyContent : 'center' ,
-                    alignSelf :'flex-end',height : 30,width :30,alignItems:'flex-end'
-                    }} source = {require('../Resources/plus.png')} ></Image>
-                    </View>
-            </SafeAreaView>
-        )
-    }
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create(
@@ -83,3 +116,5 @@ const styles = StyleSheet.create(
         },
     }
 )
+
+export default LoginView;
--- Screens/registerview.js
+++ Screens/registerview.js
@@ -7,74 +7,71 @@ import TextInputView from '../Helper/textinputview';
 import ButtonView from '../Helper/buttonview';
 import RadioForm from 'react-native-simple-radio-button';
 
-export class RegisterView extends Component
+const RegisterView = () => 
 {
-    render()
-    {
-        const genderOptions = [
-            {
-                label : 'Male',
-                value  : '1'
-            },
-            {
-                label : 'Female',
-                value : '0'
-            }
-        ];
-        return(
-            <SafeAreaView style = {styles.maincontainerStyle}>
-                <View style = {styles.containerStyle}>
-                    <Text style = {styles.textStyle}>NeoSTORE</Text>
-                    <TextInputView placeholder = 'First Name' source = {require('../Resources/user.png')}/>
-                    <TextInputView placeholder = 'Last Name' source = {require('../Resources/user.png')}/>
-                    <TextInputView placeholder = 'Email' source = {require('../Resources/email.png')}/>
-                    <TextInputView placeholder = 'Password' source = {require('../Resources/password.png')}/>
-                    <TextInputView placeholder = 'Confirm Password' source = {require('../Resources/password.png')}/>
-                    <View style = {{alignItems : 'center', flexDirection : 'row',justifyContent : 'flex-start',width : '100%'}}>
-                        <Text style = {styles.radiolabelStyle}>Gender</Text>
-                        <RadioForm radio_props = {genderOptions} 
-                        formHorizontal={true} initial={0}
-                        buttonInnerColor='white' 
-                        buttonOuterColor='white' 
-                        labelColor = 'white' 
-                        buttonColor = 'white' 
-                        selectedButtonColor = 'white'
-                        buttonSize={15}
-                        buttonOuterSize={25}
-                        labelStyle = {{position: 'relative',margin : 3,alignSelf : 'center',fontSize : 15} }
-                        selectedLabelColor = 'white'/>
-                    </View>
-                    
-
-                    <TextInputView placeholder = 'Phone Number' source = {require('../Resources/phone.png')}/>
-
-                    <View style = {styles.labelinputcontainerStyle} >
-                        <CheckBox
-                        style = {{
-                        height : 20,
-                        width : 20,marginLeft : 10}}
-                        color = 'white'
-                        borderColor = 'white' 
-                        onCheckColor = 'white' 
-                        tintColor = 'white'
-                        boxType = 'square'
-                        onTintColor = 'white'
-                        alignSelf = 'center'
-                        alignItems = 'center'/>
-                        <Text style={{alignSelf:'center', alignItems :'center',
-                        justifyContent:'flex-start',
-                        color:'white',
-                        fontSize : 15,
-                        textAlign : 'center',
-                        textAlignVertical : 'center',
-                        marginLeft : 10,
-                        fontWeight : 'bold'}}>I agree the Terms and Conditions</Text>
-                    </View>
-                    <ButtonView title = 'REGISTER'/>
+    const genderOptions = [
+        {
+            label : 'Male',
+            value  : '1'
+        },
+        {
+            label : 'Female',
+            value : '0'
+        }
+    ];
+    return(
+        <SafeAreaView style = {styles.maincontainerStyle}>
+            <View style = {styles.containerStyle}>
+                <Text style = {styles.textStyle}>NeoSTORE</Text>
+                <TextInputView placeholder = 'First Name' source = {require('../Resources/user.png')}/>
+                <TextInputView placeholder = 'Last Name' source = {require('../Resources/user.png')}/>
+                <TextInputView placeholder = 'Email' source = {require('../Resources/email.png')}/>
+                <TextInputView placeholder = 'Password' source = {require('../Resources/password.png')}/>
+                <TextInputView placeholder = 'Confirm Password' source = {require('../Resources/password.png')}/>
+                <View style = {{alignItems : 'center', flexDirection : 'row',justifyContent : 'flex-start',width : '100%'}}>
+                    <Text style = {styles.radiolabelStyle}>Gender</Text>
+                    <RadioForm radio_props = {genderOptions} 
+                    formHorizontal={true} initial={0}
+                    buttonInnerColor='white' 
+                    buttonOuterColor='white' 
+                    labelColor = 'white' 
+                    buttonColor = 'white' 
+                    selectedButtonColor = 'white'
+                    buttonSize={15}
+                    buttonOuterSize={25}
+                    labelStyle = {{position: 'relative',margin : 3,alignSelf : 'center',fontSize : 15} }
+                    selectedLabelColor = 'white'/>
                 </View>
-            </SafeAreaView>
-        )
-    }
+                
+
+                <TextInputView placeholder = 'Phone Number' source = {require('../Resources/phone.png')}/>
+
+                <View style = {styles.labelinputcontainerStyle} >
+                    <CheckBox
+                    style = {{
+                    height : 20,
+                    width : 20,marginLeft : 10}}
+                    color = 'white'
+                    borderColor = 'white' 
+                    onCheckColor = 'white' 
+                    tintColor = 'white'
+                    boxType = 'square'
+                    onTintColor = 'white'
+                    alignSelf = 'center'
+                    alignItems = 'center'/>
+                    <Text style={{alignSelf:'center', alignItems :'center',
+                    justifyContent:'flex-start',
+                    color:'white',
+                    fontSize : 15,
+                    textAlign : 'center',
+                    textAlignVertical : 'center',
+                    marginLeft : 10,
+                    fontWeight : 'bold'}}>I agree the Terms and Conditions</Text>
+                </View>
+                <ButtonView title = 'REGISTER'/>
+            </View>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create(
@@ -122,3 +119,5 @@ const styles = StyleSheet.create(
         },
     }
 )
+
+export default RegisterView;
