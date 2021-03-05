e64a611de5b9e8e7bd565ee06b2e96c45df5bb2a
Priya <kamblepriya49@gmail.com>
2021-03-05 19:11:34 +0530

    changes for storelocator.js

--- Views/Store/StoreLocator.js
+++ Views/Store/StoreLocator.js
@@ -3,15 +3,36 @@ import
 {
     View,
     SafeAreaView,
-    Text
+    Text,
+    FlatList,
+    Image
   } from "react-native"
+ import StoreLocatorViewModel  from "../../ViewModel/Store/StoreLocatorViewModel";
+ import StoreItemStyle from "./StoreItem.style";
+ import StoreLocatorListStyle from "./StoreLocatorList.style";
+import StoreItem from "./StoreItem";
 
   const StoreLocator = () => 
   {
+    const StoreItems = StoreLocatorViewModel.GetStoreLocatorList();
       return (
           <SafeAreaView>
               <View>
-                  <Text> My StoreLocator </Text>
+              <Image source = {require ('../../Assets/Images/ic_storelocator_map.png')}
+              style = {{ width: 420, height: 300 }}
+              />
+
+              <FlatList 
+                data={StoreItems}
+                keyExtractor={(item) => item.id}
+                renderItem = {data => 
+                <StoreItem
+                    title = {data.item.title}
+                    subtitle = {data.item.subtitle}
+                />}
+                style = {StoreLocatorListStyle.flatlist}
+                contentContainerStyle = {{paddingBottom : 84}}
+                />
               </View>
           </SafeAreaView>
       )
